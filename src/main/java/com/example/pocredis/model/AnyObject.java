a1eb3cf8bdb7af2a62f13a76465c5e21ae111137
RebecaCamilo <rebecacamilo3@gmail.com>
2023-12-11 13:24:56 -0300

    refac: polish imports and comments

--- src/main/java/com/example/pocredis/model/AnyObject.java
+++ src/main/java/com/example/pocredis/model/AnyObject.java
@@ -1,16 +1,10 @@
 package com.example.pocredis.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.mapping.Any;
 
 @Entity
 @Table(name = "ANY_OBJECTS")
@@ -29,9 +23,5 @@ public class AnyObject {
 	public AnyObject(final String description) {
 		this.description = description;
 	}
-	
-	//	public AnyObject att(AnyObject obj) {
-//
-//	}
-	
+
 }
