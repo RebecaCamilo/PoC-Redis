ef9600b34cbc7cd19548a9fc98974a349c31fa62
RebecaCamilo <rebecacamilo3@gmail.com>
2023-12-12 21:38:28 -0300

    feat: add cache delete annotations in update and delete methods

--- src/main/java/com/example/pocredis/service/AnyObjectService.java
+++ src/main/java/com/example/pocredis/service/AnyObjectService.java
@@ -3,6 +3,7 @@ package com.example.pocredis.service;
 import com.example.pocredis.model.AnyObject;
 import com.example.pocredis.repository.AnyObjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,12 +27,14 @@ public class AnyObjectService {
 	public AnyObject create(final String description) {
 		return repository.save(new AnyObject(description));
 	}
-	
+
+	@CacheEvict(value = "anyObjects", key = "#id")
 	public AnyObject update(Long id, String description) {
 		findById(id);
 		return repository.save(new AnyObject(id, description));
 	}
-	
+
+	@CacheEvict(value = "anyObjects", key = "#id")
 	public void delete(Long id) {
 		findById(id);
 		repository.deleteById(id);
