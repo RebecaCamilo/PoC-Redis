723dd2fde2b493a9e2fe960dc1eb9acb1cbcefa2
RebecaCamilo <rebecacamilo3@gmail.com>
2023-12-14 15:01:24 -0300

    refac: add cache expiration time and disable caching of null values

--- src/main/java/com/example/pocredis/config/RedisCacheConfig.java
+++ src/main/java/com/example/pocredis/config/RedisCacheConfig.java
@@ -2,14 +2,23 @@ package com.example.pocredis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisCacheConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager.create(connectionFactory);
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
     }
 }
